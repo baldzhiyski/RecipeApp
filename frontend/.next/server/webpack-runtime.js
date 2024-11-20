95089b34cb2fea1d3493702069662d8f81c9639f
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-11-20 17:20:22 +0100

    changes

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("03e63cda09bac979")
+/******/ 		__webpack_require__.h = () => ("4737bd59acd37957")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
