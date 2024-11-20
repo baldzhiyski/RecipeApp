282e9acdd7b4d283cfae6835a13ce4e2a59386a6
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-11-20 17:04:10 +0100

    fix

--- api/src/main/java/com/team2/client/config/SecurityConfig.java
+++ api/src/main/java/com/team2/client/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOriginPatterns(applicationProperties.getAllowedOrigins());
+                config.addAllowedOriginPattern("*");
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
